aa78fdefbb662baf7f056971c0f7b7b74e971d9d
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-07-18 22:45:47 +0300

    Initial Commit

---
+++ SwiftulSwiftUIPractice/SwiftulSwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftulSwiftUIPracticeApp.swift
+//  SwiftulSwiftUIPractice
+//
+//  Created by Michael on 7/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftulSwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
