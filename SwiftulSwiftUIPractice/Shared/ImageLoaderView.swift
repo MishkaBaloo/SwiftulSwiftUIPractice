bccfd8d2c83bae2898b14a44852076dd83ee6106
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-07-21 13:47:28 +0300

    Project Setup and Shared Components
    Rebuild spotify

---
+++ SwiftulSwiftUIPractice/Shared/ImageLoaderView.swift
@@ -0,0 +1,35 @@
+//
+//  ImageLoaderView.swift
+//  SwiftulSwiftUIPractice
+//
+//  Created by Michael on 7/18/24.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ImageLoaderView: View {
+    
+    var urlString: String =  Constans.randomImage
+    var resizingMode: ContentMode = .fill
+    
+    var body: some View {
+        Rectangle()
+            .opacity(0.001)
+            .overlay{
+                WebImage(url: URL(string: urlString))
+                    .resizable()
+                    .indicator(.activity)
+                    .aspectRatio(contentMode: resizingMode)
+                    .allowsTightening(false)
+            }
+            .clipped()
+    }
+}
+
+#Preview {
+    ImageLoaderView()
+        .clipShape(.rect(cornerRadius: 30))
+        .padding(40)
+        .padding(.vertical, 60)
+}
---
+++ SwiftulSwiftUIPractice/Shared/Product.swift
@@ -0,0 +1,25 @@
+//
+//  Product.swift
+//  SwiftulSwiftUIPractice
+//
+//  Created by Michael on 7/21/24.
+//
+
+import Foundation
+
+struct ProductArray: Codable {
+    let products: [Product]
+    let total, skip, limit: Int
+}
+
+struct Product: Codable, Identifiable {
+    let id: Int
+    let title: String
+    let description: String
+    let price: Int
+    let discountPercentage, rating: Double
+    let stock: Int
+    let brand, category: String
+    let thumbnail: String
+    let images: [String]
+}
