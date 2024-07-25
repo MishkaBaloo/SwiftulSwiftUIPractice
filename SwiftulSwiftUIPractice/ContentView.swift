cbc09d47f7cc5f97bd1a22b3cdb461e061aed609
Mykhailo Tokmakov <misha.tokmak@gmail.com>
2024-07-26 00:12:15 +0300

    Rebuild Spotify part 5/5
    Added routing, customization, open screen

--- SwiftulSwiftUIPractice/ContentView.swift
+++ SwiftulSwiftUIPractice/ContentView.swift
@@ -10,35 +10,22 @@ import SwiftfulUI
 import SwiftfulRouting
 
 struct ContentView: View {
-
-    @State private var users: [User] = []
-    @State private var products: [Product] = []
+    
+    @Environment(\.router) var router
     
     var body: some View {
-        ScrollView {
-            VStack {
-                ForEach(users) { user in
-                    Text(user.firstName)
-                        .foregroundStyle(.spotifyGreen)
+        List(content: {
+            Button("Open Spotify") {
+                router.showScreen(.fullScreenCover) { _ in
+                    SpotifyHomeView()
                 }
             }
-        }
-        .padding()
-        .task {
-            await getData()
-        }
-    }
-    
-    private func getData() async {
-        do {
-            users = try await DatabaseHelper().getUsers()
-            products = try await DatabaseHelper().getProducts()
-        } catch {
-            
-        }
+        })
     }
 }
 
 #Preview {
-    ContentView()
+    RouterView { _ in
+        ContentView()
+    }
 }
---
+++ SwiftulSwiftUIPractice/Spotify/Core/SongRowCell.swift
@@ -0,0 +1,68 @@
+//
+//  SongRowCell.swift
+//  SwiftulSwiftUIPractice
+//
+//  Created by Michael on 7/25/24.
+//
+
+import SwiftUI
+
+struct SongRowCell: View {
+    
+    var imageSize: CGFloat = 50
+    var imageName: String = Constants.randomImage
+    var title: String = "Some song name goes here"
+    var subtitle: String? = "Some artist name"
+    var onCellPressed: (() -> Void)? = nil
+    var onEllipsisPressed: (() -> Void)? = nil
+    
+    var body: some View {
+        HStack(spacing: 8) {
+            ImageLoaderView(urlString: imageName)
+                .frame(width: imageSize, height: imageSize)
+            
+            VStack(alignment: .leading, spacing: 4, content: {
+                Text(title)
+                    .font(.body)
+                    .fontWeight(.medium)
+                    .foregroundStyle(.spotifyWhite)
+                
+                if let subtitle {
+                    Text(subtitle)
+                        .font(.callout)
+                        .foregroundStyle(.spotifyLightGray)
+                }
+            })
+            .lineLimit(2)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Image(systemName: "ellipsis")
+                .font(.subheadline)
+                .foregroundStyle(.spotifyWhite)
+                .padding(16)
+                .background(Color.black.opacity(0.001))
+                .onTapGesture {
+                    onEllipsisPressed?()
+                }
+        }
+        .background(Color.black.opacity(0.001))
+        .onTapGesture {
+            onCellPressed?()
+        }
+    }
+}
+
+#Preview {
+    
+    ZStack {
+        Color.black.ignoresSafeArea()
+        
+        VStack {
+            SongRowCell()
+            SongRowCell()
+            SongRowCell()
+            SongRowCell()
+            SongRowCell()
+        }
+    }
+}
